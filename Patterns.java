6f1c0d895729cd599a635b89db9d2f05eefe4e40
Akshay <akshaysinghx9@gmail.com>
2023-12-01 19:53:05 +0530

    third commit

--- Patterns.java
+++ Patterns.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Patterns {
-    public static void printPattern(int n) {
+    public static void printPattern1(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("* ");
@@ -10,12 +10,44 @@ public class Patterns {
         }
     }
 
+    public static void printPattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printPattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
+
+        // Pattern 1
         for (int i = 0; i < t; i++) {
             int n = s.nextInt();
-            Patterns.printPattern(n);
+            Patterns.printPattern1(n);
+        }
+
+        // Pattern 2
+        for (int i = 0; i < t; i++) {
+            int n = s.nextInt();
+            Patterns.printPattern2(n);
+        }
+
+        // Pattern3
+        for (int i = 0; i < t; i++) {
+            int n = s.nextInt();
+            Patterns.printPattern3(n);
         }
         s.close();
     }
