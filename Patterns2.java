6e630915fa414d51b98edafb23e8ecbd4e823ace
Akshay <akshaysinghx9@gmail.com>
2023-12-05 15:01:09 +0530

    sixth commit

--- Patterns2.java
+++ Patterns2.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Patterns2 {
-    public static void printPattern1(int N) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
+    public static void printPattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
 
@@ -11,11 +11,76 @@ public class Patterns2 {
                 System.out.print("*");
             }
 
-            for (int j = 0; j < N - i - 1; j++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
             System.out.println("");
         }
+    }
+
+    public static void printPattern8(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printPattern9(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printPattern10(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars;
+            if (i < n) {
+                stars = i;
+            } else {
+                stars = 2 * n - i;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
     }
 
     public static void main(String[] args) {
@@ -24,7 +89,7 @@ public class Patterns2 {
 
         for (int i = 0; i < t; i++) {
             int n = s.nextInt();
-            Patterns2.printPattern1(n);
+            Patterns2.printPattern10(n);
         }
         s.close();
     }
