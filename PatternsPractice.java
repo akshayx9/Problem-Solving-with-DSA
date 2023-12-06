43d930485a33fc920f81c4ae3de7d4e96439fbf7
Akshay <akshaysinghx9@gmail.com>
2023-12-06 20:59:55 +0530

    eleventh commit

--- PatternsPractice.java
+++ PatternsPractice.java
@@ -28,12 +28,39 @@ public class PatternsPractice {
         }
     }
 
+    public void Pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void Pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void Pattern6(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
         PatternsPractice pp = new PatternsPractice();
-        pp.Pattern1(n);
+        pp.Pattern6(n);
 
         s.close();
     }
