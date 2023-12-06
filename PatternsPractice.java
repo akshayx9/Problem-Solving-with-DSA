f4dad3346939ca220d22161c4a02ce7274d920a7
Akshay <akshaysinghx9@gmail.com>
2023-12-06 21:47:38 +0530

    thirteenth commit

--- PatternsPractice.java
+++ PatternsPractice.java
@@ -91,12 +91,78 @@ public class PatternsPractice {
         }
     }
 
+    public void Pattern9(int n) {
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= 2 * n - (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            // space
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void Pattern10(int n) {
+        int stars = 0;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            if (i < n) {
+                stars = i;
+            } else {
+                stars = 2 * n - i;
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void Pattern11(int n) {
+        int start = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                start = 1;
+            } else {
+                start = 0;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
         PatternsPractice pp = new PatternsPractice();
-        pp.Pattern8(n);
+        pp.Pattern11(n);
 
         s.close();
     }
